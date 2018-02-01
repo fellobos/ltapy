b075f5e09ebdfdbd9bb039f23367f67926e70eb1
Florian Boesl <f.boesl@osram.com>
2018-02-01 14:07:25 +0100

    Add unit tests for Session class

--- lighttools/session.py
+++ lighttools/session.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 """
-Manage the connection to a LightTools session.
+Manage the connection of a Python client with LightTools.
 """
 
 import os
@@ -10,21 +12,21 @@ import winreg
 import psutil
 
 from . import _comutils
+from . import _ltapi
 from . import config
 from . import error
-from . import _ltapi
 
 
-def _get_lt_home(version):
+def _get_home_dir(version):
     """
     Return the LightTools home directory.
 
     Get the home directory of the specified LightTools `version` from the
-    Windows registry. The requested LightTools `version` must be installed
+    Windows registry.  The requested LightTools `version` must be installed
     on the system.
 
     Args:
-        version (str): The LightTools version string, e.g. "8.5.0".
+        version (str): The LightTools `version` string, e.g. "8.5.0".
 
     Returns:
         str: The LightTools home directory.
@@ -32,14 +34,15 @@ def _get_lt_home(version):
     try:
         hkey = winreg.OpenKeyEx(
             key=winreg.HKEY_LOCAL_MACHINE,
-            sub_key="SOFTWARE\\Optical Research Associates\\LightTools\\{}\\Environment".format(
-                version
-            ),
+            sub_key=(
+                "SOFTWARE\\Optical Research Associates\\LightTools"
+                "\\{}\\Environment"
+            ).format(version),
         )
     except OSError:
         msg = (
             "Couldn't find home directory of LighTools version {!r}. Please "
-            "check if the requested LightTools version is installed."
+            "check if the requested version of LightTools is installed."
         )
         raise ValueError(msg.format(version))
     else:
@@ -50,34 +53,21 @@ def _get_lt_home(version):
 class Session(object):
 
     """
-    Create a connection to a LightTools session.
+    Connect to a running LightTools session.
 
-    The connection to LightTools can be established in the following ways:
-
-    * Connect to an already running LightTools session by specifying the
-      PID of the LightTools process.
-
-    * Start a new LightTools session and connect to that session by
-      specifying the display name of the desired LightTools version (see
-      Session.new() for further details).
-
-    The generated LighTools API object (ltapi) has advanced error handling
-    capabilities. Instead of just returning the error code (the default
-    behaviour), the enhanced LightTools API object checks the return value
-    of an API function call and raises an APIError exception if the
-    function returns with an error.
+    Create a connection to a running LightTools session, using Python as
+    client program.  The `lt` attribute provides a handle to the
+    connected session.
 
     Args:
         pid (int, optional): The process ID of an already running
-            LightTools session. If pid is given connect to that specific
-            session, otherwise connect to an arbitrary, running LightTools
-            session.
+            LightTools session.  If `pid` is given connect to that specific
+            session, otherwise connect to an arbitrary session.
         timeout (int, optional): The time limit in seconds after which a
             connection attempt to LightTools is aborted.
 
     Attributes:
-        ltapi (ILTAPIx): A handle to the LightTools session.
-        pid (int): The PID of the LightTools process.
+        lt (ILTAPIx): A handle to the LightTools session.
 
     Raises:
         TimeOutError: If a connection attempt with LightTools was aborted
@@ -87,49 +77,40 @@ class Session(object):
         Connect to a running LightTools session by specifying the PID of
         the LightTools process.
 
-        >>> ses = lts.Session(pid=5642)
-        >>> lt = ses.ltapi
-        >>> lt.Message("Connected to LightTools, PID: {}".format(ses.pid))
-
-        Start a new LightTools session (by specifiying the display name of
-        the desired version) and connect to that session.
-
-        >>> lts.installed_versions()
-        ['LightTools(64) 8.3.2', 'LightTools(64) 8.4.0']
-        >>> ses = lts.Session.new(version="LightTools(64) 8.4.0")
-        >>> lt = ses.ltapi
+        >>> ses = Session(pid=5642)
+        >>> lt = ses.lt
+        >>> lt.Message("Successfully connected to LightTools!")
     """
 
     def __init__(self, pid=None, timeout=config.TIMEOUT, _rebuild=False):
-        self.pid = pid
+        self._pid = pid
         self._timeout = timeout
         self._rebuild = _rebuild
 
         # Get a handle to the LightTools session.
         comobj = self._get_COM_object()
-        self.ltapi = _ltapi.LTAPI(comobj, self._rebuild)
+        self.lt = _ltapi.LTAPI(comobj, self._rebuild)
 
-        # Set the process ID if the connection was established with an
-        # arbitrary LightTools session.
-        if not self.pid:
-            self.pid = self.ltapi.GetServerID()
+        # Get the process ID if connected to an arbitrary LightTools session.
+        if not self._pid:
+            self._pid = self.lt.GetServerID()
 
     def _get_COM_object(self):
         """
-        Return a LightTools COM server object from the running object table.
+        Return a LightTools COM server object from the Running Object Table.
 
         Returns:
-            comobj (PyIUnknown): The LightTools COM server object. If no
+            comobj (PyIUnknown): The LightTools COM server object.  If no
                 process ID was given return an arbitrary LightTools COM object
-                from the running object table.
+                from the Running Object Table.
         """
         prefix = "LightTools API Server | "
 
-        if self.pid:
-            if not psutil.pid_exists(self.pid):
-                msg = "Couldn't find LightTools process with PID: {}"
-                raise ValueError(msg.format(self.pid))
-            prefix += str(self.pid)
+        if self._pid:
+            if not psutil.pid_exists(self._pid):
+                msg = "Couldn't find a LightTools process with PID {}."
+                raise ValueError(msg.format(self._pid))
+            prefix += str(self._pid)
 
         start_time = time.time()
         connection_attempt_timed_out = (
@@ -144,18 +125,21 @@ class Session(object):
                 if name.startswith(prefix):
                     return comobjs[name]
         else:
-            msg = ("Connection attempt with LightTools aborted due to "
-                   "timeout: {} sec")
+            msg = (
+                "Couldn't establish a connection to LightTools within {} "
+                "seconds. Connection attempt aborted."
+            )
             raise error.TimeOutError(msg.format(self._timeout))
 
     @classmethod
-    def new(cls, version=config.VERSION, timeout=config.TIMEOUT, _rebuild=False):
+    def new(cls, version=config.VERSION, timeout=config.TIMEOUT,
+            _rebuild=False):
         """
-        Start a new instance of LightTools and connect to that session.
+        Start a new LightTools instance and connect to that session.
 
         Args:
             version (str, optional): The LightTools version to be started.
-            timeout (int, optional): Time limit in seconds after which the
+            timeout (int, optional): The time limit in seconds after which the
                 connection attempt to LightTools is aborted.
 
         Returns:
@@ -170,8 +154,9 @@ class Session(object):
             Start a new LightTools instance and connect to that session:
 
         >>> ses = Session.new(version="8.5.0")
-        >>> lt = ses.ltapi
+        >>> lt = ses.lt
+        >>> lt.Message("Successfully connected to LightTools!")
         """
-        lt_home = _get_lt_home(version)
-        proc = subprocess.Popen(os.path.join(lt_home, "lt.exe"))
+        home_dir = _get_home_dir(version)
+        proc = subprocess.Popen(os.path.join(home_dir, "lt.exe"))
         return cls(proc.pid, timeout, _rebuild)
--- tests/test_session.py
+++ tests/test_session.py
@@ -1,15 +1,34 @@
-import lighttools.session
+import os
+import subprocess
+
 import lighttools.config
+import lighttools.error
+import lighttools.session
 
 import pytest
 
 
+def test_connect_to_running_session():
+    with pytest.raises(ValueError):
+        lighttools.session.Session(pid=-1)
+
+    with pytest.raises(lighttools.error.TimeOutError):
+        lighttools.session.Session(timeout=-1)
+
+    home_dir = lighttools.session._get_home_dir(lighttools.config.VERSION)
+    proc = subprocess.Popen(os.path.join(home_dir, "lt.exe"))
+    ses = lighttools.session.Session(pid=proc.pid)
+    lt = ses.lt
+    assert lt.GetServerID() == proc.pid
+    proc.kill()
+
+
 def test_start_new_session():
+    with pytest.raises(ValueError):
+        ses = lighttools.session.Session.new(version="99.9.9")
+
     ses = lighttools.session.Session.new()
-    lt = ses.ltapi
+    lt = ses.lt
     version = lt.Version(0)
     lt.Cmd("Exit")
     assert version == lighttools.config.VERSION
-
-    with pytest.raises(ValueError):
-        ses = lighttools.session.Session.new(version="99.9.9")
