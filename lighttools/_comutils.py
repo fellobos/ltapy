65de3b12ceb3cd7a6e69153d5f97f12c8969a17f
Florian Boesl <f.boesl@osram.com>
2018-02-01 16:30:26 +0100

    Polish code and update docstrings

--- lighttools/_comutils.py
+++ lighttools/_comutils.py
@@ -1,22 +1,25 @@
+# -*- coding: utf-8 -*-
+
 """
 Utility methods for client-side COM support.
 
-COM client support is the ability to manipulate COM server objects via
-their exposed interface. Automation objects (e.g. LightTools) are COM
-server objects that expose their methods and properties using the
-IDispatch interface. This interface enables them to be accessed by
-automation clients, such as Visual Basic or Python.
+The `RunningObjectTable` class simplifies the access to the Running
+Object Table (ROT) and to the COM objects that are registered there.
+Furthermore, this module defines a few helper functions to support
+early-bound automation for IDispatch based COM objects.
 
-All COM server objects that are currently running on the computer are
-registered in the running object table (ROT). The ROT is a globally
-accessible look-up table that keeps track of those objects. The
-LightTools API also uses the Microsoft Windows COM interface and
-registers its objects with the ROT.
+Notes:
+    COM client support is the ability to manipulate COM server objects
+    via their exposed interface.  Automation objects (e.g. LightTools)
+    are COM server objects that expose their methods and properties
+    using the IDispatch interface.  This interface enables them to be
+    accessed by automation clients, such as Visual Basic or Python.
 
-The `RunningObjectTable` class provides easy access to the ROT and to
-the COM objects that are registered there. Furthermore, this module
-defines a few helper functions in order to support early-bound
-automation for IDispatch based COM objects.
+    All COM server objects that are currently running on the computer
+    are registered in the ROT.  The ROT is a globally accessible
+    look-up table that keeps track of those objects.  The LightTools
+    API also uses the Microsoft Windows COM interface and registers its
+    objects in the ROT.
 """
 
 import os
@@ -28,14 +31,13 @@ import win32com.client
 class RunningObjectTable(object):
 
     """
-    Provide access to COM objects that are registered in the running
-    object table (ROT).
+    Provide access to COM objects listed in the Running Object Table.
 
     Examples:
-        Show the contents of the running object table:
+        Show the contents of the Running Object Table:
 
         >>> rot = RunningObjectTable()
-        >>> rot.show()
+        >>> print(rot)
         LightTools API Server | 1912
         LightTools API Server | 6940
         !{0006F03A-0000-0000-C000-000000000046}
@@ -70,27 +72,27 @@ class RunningObjectTable(object):
         Return the display name of the given moniker.
 
         Args:
-            moniker (PyIMoniker): The moniker identifying the COM object.
+            moniker (PyIMoniker): The `moniker` identifying the COM object.
 
         Returns:
-            str: The display name of the moniker.
+            str: The display name of the `moniker`.
         """
         return moniker.GetDisplayName(pythoncom.CreateBindCtx(0), None)
 
     def get_object(self, name):
         """
-        Return a COM object from the running object table.
+        Return a COM object from the Running Object Table.
 
         Args:
-            name (str): The display name of the moniker that identifies
+            name (str): The display `name` of the moniker that identifies
                 the requested COM object.
 
         Returns:
             PyIUnknown: The requested COM object.
 
         Raises:
-            ValueError: If a COM object with the given moniker name could not
-                be found.
+            ValueError: If a COM object with the given moniker `name` could
+                not be found.
         """
         for moniker in self.rot:
             moniker_name = self._get_moniker_name(moniker)
@@ -102,10 +104,7 @@ class RunningObjectTable(object):
 
     def get_objects(self):
         """
-        Return all COM objects from the running object table.
-
-        Args:
-            None
+        Return all COM objects from the Running Object Table.
 
         Returns:
             dict: A mapping that contains all running COM objects as
@@ -115,7 +114,7 @@ class RunningObjectTable(object):
         for moniker in self.rot:
             moniker_name = self._get_moniker_name(moniker)
             # A race condition might occur if a COM application is closed
-            # while the Running Object Table is scanned. There is therefore
+            # while the Running Object Table is scanned.  There is therefore
             # a chance that the object identified by moniker is not alive
             # any more.
             try:
